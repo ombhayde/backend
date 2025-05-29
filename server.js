4d69d19d6601367e0bf790d3d1614470381a30f7
ombhayde <135888892+ombhayde@users.noreply.github.com>
2025-05-29 14:42:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT ||5000;
 const nodemailer = require('nodemailer');
 // Middleware
 app.use(cors());
